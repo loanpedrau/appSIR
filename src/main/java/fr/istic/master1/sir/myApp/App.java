3ffa1bef1eeb5af8c37f0885ac784a041dc45463
Pedrau Loan <loan.pedrau@etudiant.univ-rennes1.fr>
2021-01-27 18:30:38 +0100

    test add code jenkins

--- src/main/java/fr/istic/master1/sir/myApp/App.java
+++ src/main/java/fr/istic/master1/sir/myApp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-	System.out.println( "Hello World! Bonjour !" );
+	System.out.println( "Hello World! Bonjour ! Ajout de code test" );
     }
 }
